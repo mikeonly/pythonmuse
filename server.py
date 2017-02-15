b03855a71c0cd791c2ef8afdfdac7826e5752c16
Michael Flaks <misha.flaks@gmail.com>
2017-02-15 16:29:41 -0700

    Added Bokeh plotting from one channel

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import logging
 import sys
 
 from multiprocessing import Process
+from threading import Thread
 
 # Modules for MuseServer.
 from liblo import ServerThread, make_method
@@ -30,7 +31,6 @@ formatter = logging.Formatter(
 shandler = logging.StreamHandler(sys.stderr)
 shandler.setFormatter(formatter)
 logger.addHandler(shandler)
-
 
 class MuseServer(ServerThread):
     # Listen for messages
@@ -96,10 +96,10 @@ class Grapher(Process):
                                             time=[]))
         self.fig = figure()
         self.fig.line(source=self.source, x='time', y='LAUX',
-                      line_width=2, alpha=0.85, color='red')
+                      line_width=2, alpha=0.85, color='red')  
         self.fig.line(source=self.source, x='time', y='RAUX',
                       line_width=2, alpha=0.85, color='red')
-        self.doc.add_root(self.fig)
+        # self.doc.add_root(self.fig)
         self.session = push_session(self.doc)
 
     def run(self):
@@ -108,27 +108,28 @@ class Grapher(Process):
         with each data channel: timestamp, LAUX and RAUX, but can be more.'''
         # Receive arguments from pipe for the first time. It iniaites lists for
         # further appending.
-        self.timestamp0, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
-        self.timestamp += [self.timestamp0]  # Only for the first time.
-        self.LAUX += [LAUX]
-        self.RAUX += [RAUX]
-        ct = 0  # Counter for debug.
-        while True:
-            # Get all possible arguemnts.
-            timestamp, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
-            self.timestamp += [timestamp]
-            self.RAUX += [RAUX]
-            self.LAUX += [LAUX]
-            ct += 1  # Create a counting variable.
-            if ct % 100 == 0:  # Update the plot on each 100th packet.
-                self.update()
+        source = ColumnDataSource(data=dict(x=[0], y=[0]))
 
-    @gen.coroutine
-    def update(self):
-        time = [t - self.timestamp0 for t in self.timestamp]
-        self.source.stream(dict(time=[time], LAUX=[self.LAUX],
-                                RAUX=[self.RAUX]), 100)
-        # Clean arrays after updating.
-        self.timestamp = []
-        self.LAUX = []
-        self.RAUX = []
+        p = figure()
+        # self.timestamp0, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
+        r2 = p.line(source=source, x='x', y='y',
+                    line_width=2, alpha=0.85, color='red')
+        # self.timestamp += [self.timestamp0]  # Only for the first time.
+        # # open a session to keep our local document in sync with server
+        session = push_session(curdoc())
+
+        timestamp0, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
+        source.stream(dict(x=[0], y=[LAUX]))
+
+        def update():
+            # On update, recieve arguments from OSC channel.
+            timestamp, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
+            # Update source with the recieved variables.
+            time = timestamp-timestamp0
+            source.stream(dict(x=[time], y=[LAUX]))
+
+        curdoc().add_periodic_callback(update, 2)
+
+        session.show(p) # open the document in a browser
+
+        session.loop_until_closed() # run forever
