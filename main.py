57752869fa61d93fe39cd9ae6850d75732dba246
Michael Flaks <misha.flaks@gmail.com>
2017-04-24 18:48:51 -0600

    Rewrite for LSL

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from multiprocessing import Pipe
 import time
-from server import *
+import os
+import logging
+import logging.config
+import yaml
+
+from server import MuseServer, Saver
 
 
-# Create pipes for connection between MuseServer, Saver and Grapher.
-# Duplex is false, for `conn1, conn2 = Pipe(duplex=False)` guarantees that
-# conn1 can only receive data and conn2 can only send data.
-saver_pipe = Pipe(duplex=False)
-grapher_pipe = Pipe(duplex=False)
+def setup_logging(
+        default_path='logging.yaml', default_level=logging.INFO,
+        env_key='LOG_CFG'):
+    """Setup logging configuration
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
-# MuseServer applies .send() method to eeg_pipes once eeg_callback function is
-# called.
-relay = MuseServer(port=5000, eeg_pipes=[saver_pipe, grapher_pipe])
-saver = Saver(pipe=saver_pipe, name='Saver', savefile='data.csv')
-grapher = Grapher(pipe=grapher_pipe, name='Grapher')
+
+def main():
+    relay = MuseServer(port=5000)
+    saver = Saver(name='Saver', savefile='data.csv')
+    relay.start()
+    saver.start()
+
 
 # Starts all the processes.
 if __name__ == '__main__':
-    relay.start()
-    saver.start()
-    grapher.start()
-    time.sleep(100)
+    setup_logging()
+    logging.info('Started')
+    main()
+    logging.info('Main initialized, waiting until done')
+    time.sleep(200)
+    logging.info('Main finished')
--- server.py
+++ server.py
@@ -1,86 +1,138 @@
 # relay.py recieves the OSC stream and relays packets to other processes.
-import time
 import logging
-import sys
 
+# Makes all process independent by spawning separate Python instances for each.
 from multiprocessing import Process
-from threading import Thread
 
-# Modules for MuseServer.
+# Creates and resolves streams.
+import pylsl
+from pylsl import StreamInfo, StreamOutlet, local_clock, StreamInlet
+
+# Module for MuseServer that reads OSC stream.
 from liblo import ServerThread, make_method
 
 # Modules for Saver.
 import numpy as np
 
-# Modules for Grapher.
-from bokeh.models import ColumnDataSource
-from bokeh.plotting import curdoc, figure
-from bokeh.client import push_session
-
-from functools import partial
-from tornado import gen
-
-
-# Create a logger for debug purposes.
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name=__name__)
-# Specify logger format.
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
-# Stream handler for console output
-shandler = logging.StreamHandler(sys.stderr)
-shandler.setFormatter(formatter)
-logger.addHandler(shandler)
 
 class MuseServer(ServerThread):
     # Listen for messages
-    """MuseServer receives OSC packets from TCP stream and sends them to the
-    specified ouput pipes."""
-    def __init__(self, port, eeg_pipes):
+    """MuseServer receives OSC packets from TCP stream and makes them available
+    at the specified EEG LSL stream."""
+
+    def __init__(self, port, num_channels=6, freq=500, logger=None):
         ServerThread.__init__(self, port)
-        # In and out are relative to the obj.
-        input_ps, self.eeg_output_ps = zip(*eeg_pipes)
+        self.logger = logger or logging.getLogger('server.MuseServer')
+
+        # Create pylsl stream for EEG
+        CHUNK_SIZE = 32
+        OUT_BUFFER_SIZE = 360  # in seconds
+        self.info = StreamInfo('Muse', 'EEG', num_channels,
+                               freq, 'float32', 'muse-b0e5')
+        self.info.desc().append_child_value('manufacturer', 'Interaxon')
+        self.channels = self.info.desc().append_child('channels')
+        for channel in ['LAUX', 'TP9', 'AF7', 'AF8', 'TP10', 'RAUX']:
+            self.channels.append_child('channel') \
+                .append_child_value('label', channel) \
+                .append_child_value('unit', 'microvolts') \
+                .append_child_value('type', 'EEG')
+        self.outlet = StreamOutlet(self.info, CHUNK_SIZE, OUT_BUFFER_SIZE)
+        self.logger.info('Created EEG outlet: {}'.format(self.info.desc()))
 
     # eeg_callback is called whenever a packet is received with the specified
     # /muse/eeg path.
     @make_method('/muse/eeg', 'ffffff')
     def eeg_callback(self, path, args):
-        timestamp = time.time()  # POSIX time format
-        packet = [timestamp] + args  # Append time to packets
-        for pipe in self.eeg_output_ps:
-            pipe.send(packet)
+        timestamp = local_clock()
+        self.outlet.push_sample(args, timestamp)
+        self.logger.info('Sent sample')
 
 
 class Saver(Process):
-    "Saver reads the input pipe and save the data into a file."
-    def __init__(self, name, pipe, savefile=None, buffer_size=100):
-        # Saver reads one passed pipe for now. Maybe I should create a
-        # decorator to allow for more pipe being read and saved?
-        super(Saver, self).__init__(name=name, daemon=True)
+    """Saver reads input from available LSL stream with EEG type and saves
+     recieved data into the `savefile` file."""
 
-        self.input_p, output_p = pipe  # In and out are relative to the obj.
-        self.buffer = []  # Buffer for data recieved by pipe.
+    def __init__(self, name, savefile=None, buffer_size=100, logger=None):
+        super(Saver, self).__init__(name=name, daemon=True)
+        self.logger = logger or logging.getLogger('server.Saver')
+
         self.savefile = savefile or name + '.csv'
         self.buffer_size = buffer_size
+        self.logger.info('Creating and instance of Saver')
+
+        self.resolve()
 
     def run(self):
         # While running, keep the output file open.
         with open(self.savefile, mode='ab+') as f:
             while True:
-                # Read from the pipe. Waits until pipe is not empty.
-                self.buffer += [self.input_p.recv()]
-                if len(self.buffer) > self.buffer_size:
-                    self.save(f, self.buffer)
-                    self.buffer = []
+                try:
+                    chunk, timestamps = self.inlet.pull_chunk(
+                        timeout=10.0, max_samples=self.buffer_size)
+                    self.logger.info('Recieved packet from LSL stream')
+                    self.buffer += [chunk + [timestamps]]
+                    if len(self.buffer) > self.buffer_size:
+                        self.save(f, self.buffer)
+                        self.logger.info('Wrote buffer to the file')
+                        self.buffer = []
+                except pylsl.LostError as e:
+                    self.logger.warn(e)
+                    self.logger.info(
+                        'Stream source has been lost, trying to resolve')
+                    self.resolve()
+                    continue
+
+    def resolve(self):
+        self.logger.info('Resolving LSL stream for EEG')
+        # Resolve streams in LSL networks with type EEG, timeout in seconds.
+        streams = pylsl.resolve_byprop('type', 'EEG', minimum=1, timeout=10)
+        self.logger.info('Resolved EEG streams:\n {}'.format(streams))
+        self.inlet = StreamInlet(streams[0])
+        self.logger.info('Created stream inlet {}'.format(self.inlet))
 
     def save(self, f, data):
         # Save args into a csv file.
         np.savetxt(f, data, delimiter=',', newline='\n')
 
 
+class Fourier(Process):
+    '''Fourier class resolves EEG LSL stream, does FFT on data and makes
+    Fourier LSL stream available for other processes'''
+
+    def __init__(self, name, pipe):
+        super(Fourier, self).__init__(name=name, daemon=True)
+
+        # Create pylsl stream for Fourier.
+        CHUNK_SIZE = 32
+        OUT_BUFFER_SIZE = 360  # in seconds
+        self.info = StreamInfo('Muse', 'Fourier', 'float32', 'muse-b0e5')
+        self.info.desc().append_child_value('manufacturer', 'Interaxon')
+        self.channels = self.info.desc().append_child('channels')
+        for channel in ['LAUX', 'TP9', 'AF7', 'AF8', 'TP10', 'RAUX']:
+            self.channels.append_child('channel') \
+                .append_child_value('label', channel) \
+                .append_child_value('unit', 'microvolts') \
+                .append_child_value('type', 'Fourier')
+        self.outlet = StreamOutlet(self.info, CHUNK_SIZE, OUT_BUFFER_SIZE)
+        self.logger.info('Created Fourier outlet: {}'.format(self.info.desc()))
+
+    def run(self):
+        '''While running, recieve raw EEG data and do FFT on it.'''
+        while True:
+            chunk, timestamps = self.inlet.pull_chunk(
+                timeout=10.0, max_samples=self.buffer_size)
+            sample = []
+            fft = sp.fft(signal)
+            spectrum = abs(fft)[:NUM_SAMPLES/2]
+            power = spectrum**2
+            bins = simps(np.split(power, NUM_BINS))
+        self.outlet.push_sample(sample)
+
+
 class Grapher(Process):
     """Grapher gets data into input pipe, create Bokeh backend and inputs
     received data into the Bokeh server."""
+
     def __init__(self, name, pipe):
         super(Grapher, self).__init__(name=name, daemon=True)
         # multiprocessing config.
@@ -112,9 +164,9 @@ class Grapher(Process):
             # On update, recieve arguments from OSC channel
             timestamp, LAUX, TP9, AF7, AF8, TP10, RAUX = self.input_p.recv()
             # Update source with the recieved variables.
-            time = timestamp-timestamp0
+            time = timestamp - timestamp0
             source.stream(dict(x=[time], y=[LAUX]), rollover=1000)
 
         curdoc().add_periodic_callback(update, 2)
-        session.show(fig) # open the document in a browser
-        session.loop_until_closed() # run forever
+        session.show(fig)  # open the document in a browser
+        session.loop_until_closed()  # run forever
